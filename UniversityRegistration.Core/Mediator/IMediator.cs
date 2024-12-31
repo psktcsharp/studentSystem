59ba594faa1bf2f0658b266031c712bd59f1ae36
ammar <safeerlibya@gmail.com>
2025-01-01 00:52:17 +0200

    Added mediator interface and base command/query interfaces.

---
+++ UniversityRegistration.Core/Mediator/IMediator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniversityRegistration.Core.Mediator
+{
+    public interface IMediator
+    {
+        Task<TResponse> Send<TResponse>(IRequest<TResponse> request);
+    }
+
+    public interface IRequest<TResponse>
+    {
+
+    }
+}
---
+++ UniversityRegistration.Core/Mediator/Queries/IQuery.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UniversityRegistration.Core.Mediator;
+
+namespace UniversityRegistration.Core.Mediator.Queries
+{
+    public interface IQuery<TResponse> : IRequest<TResponse>
+    {
+
+    }
+}
